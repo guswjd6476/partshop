1cfdc896685654461dde7deba4562ba0f3054fff
guswjd6476 <sspp4555@naver.com>
2023-06-12 16:18:10 +0900

    0612

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -20,7 +20,7 @@ module.exports = function(app) {
     app.use(
         //createProxyMiddleware('/api', {
 		createProxyMiddleware('/api',{
-            target: 'https://port-0-partshopback-lme62alhk7lvdw.sel4.cloudtype.app',
+            target: 'https://port-0-partshopback-lme62alhk7lvdw.sel4.cloudtype.app:5000/',
             changeOrigin: true,
         })
     );
