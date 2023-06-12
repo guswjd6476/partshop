d1e85755ec46ba99d2191c95e889dd1f81b93494
guswjd6476 <sspp4555@naver.com>
2023-06-12 15:18:01 +0900

    0612

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,4 +1,18 @@
 
+// // src/setupProxy.js
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function(app) {
+//     app.use(
+//         //createProxyMiddleware('/api', {
+// 		createProxyMiddleware('/api',{
+// 			   target: 'http://localhost:5000/',
+//             changeOrigin: true,
+//         })
+//     );
+// };
+
+
 // src/setupProxy.js
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
@@ -6,7 +20,7 @@ module.exports = function(app) {
     app.use(
         //createProxyMiddleware('/api', {
 		createProxyMiddleware('/api',{
-            target: 'https://port-0-partshopback-lme62alhk7lvdw.sel4.cloudtype.app/5000',
+            target: 'https://port-0-partshopback-lme62alhk7lvdw.sel4.cloudtype.app:5000',
             changeOrigin: true,
         })
     );
