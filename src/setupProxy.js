6e66910d323788bbcd32d94bc1ac017c7ce3f574
guswjd6476 <sspp4555@naver.com>
2023-06-12 12:30:26 +0900

    0612

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+
+// src/setupProxy.js
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        //createProxyMiddleware('/api', {
+		createProxyMiddleware('/api',{
+            target: 'http://localhost:5000/',
+            changeOrigin: true,
+        })
+    );
+};
