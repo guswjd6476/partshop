780f898291b0326329f70dc88cfcf9a3bb61ab0b
guswjd6476 <sspp4555@naver.com>
2023-06-12 17:04:41 +0900

    0612

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -19,8 +19,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         //createProxyMiddleware('/api', {
-		createProxyMiddleware('https://web-partshop-lme62alhk7lvdw.sel4.cloudtype.app/api',{
-            target: 'partshopback:5000/',
+		createProxyMiddleware('/api',{
+            target: 'http://localhost:5000/',
             changeOrigin: true,
         })
     );
