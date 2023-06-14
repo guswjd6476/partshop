72d87ff4e3758ffcee0752d63581b5de89effe6b
guswjd6476 <sspp4555@naver.com>
2023-06-14 12:31:29 +0900

    0614

--- src/components/Main/Mainevent.js
+++ src/components/Main/Mainevent.js
@@ -16,21 +16,20 @@ function Mainevent({cates}) {
 
   return (
     <div className='iotallcon'>
-    <Pagetitle value={'진행중인 이벤트'} main={true}/>
-    <Mainlinkto cates={cates} num={4} value={'이벤트'} color={'b'}/>
+      <Pagetitle value={'진행중인 이벤트'} main={true}/>
+      <Mainlinkto cates={cates} num={4} value={'이벤트'} color={'b'}/>
       <div className='iotcontains'>
-        {iotlist&&iotlist.map(value=>
-        <div className='mainiotwrap' key={value.id}>
+        {iotlist && iotlist.slice(0, 2).map(value =>
+          <div className='mainiotwrap' key={value.id}>
             <div className='mainIotimg'>
-                <img src=   {value.thumb}/>
+              <img src={value.thumb} />
             </div>
             <div>
-                <span>[{value.brand}]{value.pName}</span>
-                <p>{value.pDetail}</p>
+              <span>[{value.brand}]{value.pName}</span>
+              <p>{value.pDetail}</p>
             </div>
-          
-            
-        </div>)}
+          </div>
+        )}
       </div>
     </div>
   );
