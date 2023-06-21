638d30055aaa4d4c2980f95524649e3a6f67c100
guswjd6476 <sspp4555@naver.com>
2023-06-21 16:25:05 +0900

    0621

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,40 @@
 
-import { AppstoreOutlined,  ShoppingCartOutlined,UserAddOutlined,ExclamationCircleFilled } from '@ant-design/icons';
-import { Menu,Modal  } from 'antd';
+import { AppstoreOutlined, UserOutlined, ShoppingCartOutlined,SearchOutlined,ExclamationCircleFilled,HeartOutlined } from '@ant-design/icons';
+import { Menu,Modal ,Dropdown,Popover } from 'antd';
 import {  Link,useLocation,} from 'react-router-dom';
 import logo from './Image/logo.png'
 import AllSearchComponent from './components/Allsearch/AllSearchComponent'
+import { useState,useEffect } from 'react';
 const { confirm } = Modal;
 
 
 const Header = ({isLoggedIn,admin,userInfo,setFilter,sb,setSb,cates,filter}) => {
   
+  const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+  const [searchon, setSearchon] = useState(false)
+  useEffect(() => {
+    let prevScrollY = 0;
+  
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+  
+      if (currentScrollY !== prevScrollY) {
+        if (currentScrollY > 80) {
+          setIsHeaderFixed(true);
+        } else {
+          setIsHeaderFixed(false);
+        }
+  
+        prevScrollY = currentScrollY;
+      }
+    };
+  
+    window.addEventListener('scroll', handleScroll);
+  
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   const addedKeys = [];
   const newItems = cates&&cates.map((item) => {
     const key = `/${item.category}`;
@@ -19,7 +45,7 @@ const Header = ({isLoggedIn,admin,userInfo,setFilter,sb,setSb,cates,filter}) => 
     }
     addedKeys.push(key);
 
-    
+   
     return {
       label: <Link to={`/${item.category}`}>{item.category}</Link>,
       key,
@@ -28,7 +54,6 @@ const Header = ({isLoggedIn,admin,userInfo,setFilter,sb,setSb,cates,filter}) => 
 
     };
   });
-  // const navigate = useNavigate();
   const location = useLocation()
   const pathKeys = location.pathname.split('/').filter(Boolean)
 
@@ -47,76 +72,99 @@ const Header = ({isLoggedIn,admin,userInfo,setFilter,sb,setSb,cates,filter}) => 
       },
     });
   };
-  return (
-<div className='header '>
-  <div className='header_top_bg'>
-    <ul className='header_top_box displaybox'>
-      {userInfo&&userInfo[1]== 1 ? 
-      <li>
-        <Link to='/Admin'>
-        관리자 입니다
-        </Link>
-      </li> 
-      :'' 
-    }
-      {!isLoggedIn ? 
-    <li>
-    <Link to='/Login'>
+  const items = [
+    {
+      label: <Link  to={!isLoggedIn ? '/Login':'/Mypage'} className='function_head_box login'>
+     마이페이지
+    </Link>,
+      key: '0',
+    },
+    {
+      label:  <div style={{marginLeft:'20px'}}>
+      <Link to='/center'>
+      고객센터
+      </Link>
+    </div>,
+      key: '1',
+    },
+    {
+      label: !isLoggedIn ? <div> 
+     <Link to='/Login'>
     로그인
     </Link>
-  </li>
-  :''  
-    }
-        {!isLoggedIn ? 
-      <li style={{marginLeft:'20px'}}>
-        <Link to='/Signup'>
-        회원가입
+    </div> : <div onClick={showConfirm} style={{marginLeft:'20px'}}> 
+      <a>
+      로그아웃
+      </a>
+    </div> ,
+      key: '3',
+    },
+    userInfo&&userInfo[1]== 1 ? {
+      label: <div>
+       <Link to='/Admin'>
+        관리자 입니다
         </Link>
-      </li>
-      :
-      ''
-      }
-      {isLoggedIn ? 
-      <li onClick={showConfirm} style={{marginLeft:'20px'}}>
-        <a>
-        로그아웃
-        </a>
-      </li>
-      :
-      ''
-      }
-      <li style={{marginLeft:'20px'}}>
-        <Link to='/center'>
-        고객센터
-        </Link>
-      </li>
-    </ul>
-  </div>
+    </div>,
+      key: '4',
+    }:''
+  ];
+  const [open, setOpen] = useState(false);
+  const hide = () => {
+    setOpen(false);
+  };
+  const handleOpenChange = (newOpen) => {
+    setOpen(newOpen);
+  };
+  return (
   
- <div className='header_box displaybox'>
-    <Link to='/' className='logo'>
+<div className={isHeaderFixed ?'header fix':'header ' }>
+ 
+ {/* <div className='header_bottom_bg'>
+ </div> */}
+  <div className='header_wrap displaybox'>
+ <Link to='/' className='logo'>
         <img src={logo} alt='logo'/>
     </Link>
-    <div className='search_bar'>
-      <AllSearchComponent filter={filter} userId={admin&&admin.split(',')[0]} setSb={setSb} sb={sb} setFilter={setFilter}/>
+  <Menu className='navigation ' 
+selectedKeys={[...pathKeys, `/${pathKeys[0]}`]} mode="horizontal" items={newItems} />
+ <ul className='function_head'>
+    <div className='function_head_box login'>
+      
+      <Popover
+      className='searchpop'
+      content={
+      <div className='search_bar'>
+        <div className='searchbar_title'>검색어를 입력해주세요</div>
+        <AllSearchComponent onClick={hide} filter={filter} userId={admin&&admin.split(',')[0]} setSb={setSb} sb={sb} setFilter={setFilter}/>
+      </div>
+    }
+      trigger="click"
+      open={open}
+      onOpenChange={handleOpenChange}
+    >
+     <div onClick={e=>setSearchon(!searchon)} className='search_btn'>
+        <SearchOutlined />
+      </div>
+    </Popover>
     </div>
-    <ul className='function_head'>
+        <Dropdown
+        menu={{
+          items,
+        }}
+        trigger={['click']}
+      >
+        <UserOutlined />
+      </Dropdown>
+      
         <Link  to={!isLoggedIn ? '/Login':'/Mypage'} className='function_head_box login'>
-          <UserAddOutlined />
-          <div>마이페이지</div>
+        <HeartOutlined />
         </Link>
         <Link to={'/Cart'} className='function_head_box cart'>
           <ShoppingCartOutlined />
-          <div>장바구니</div>
         </Link>
     </ul>
- </div>
- <div className='header_bottom_bg'>
-
-  <Menu className='navigation displaybox' 
-selectedKeys={[...pathKeys, `/${pathKeys[0]}`]} mode="horizontal" items={newItems} />
-
   </div>
+ 
   </div>
   )
 };
