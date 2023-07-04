8eca53ef2789e11234c66b2850855d6866146391
guswjd6476 <sspp4555@naver.com>
2023-07-04 15:33:46 +0900

    0704

--- src/components/shop/productpg/Productdetailsub.js
+++ src/components/shop/productpg/Productdetailsub.js
@@ -1,68 +1,119 @@
-import { Tabs,theme } from "antd";
+import { Anchor} from "antd";
 import StickyBox from 'react-sticky-box';
 import Productdelivery from "./Productdelivery";
 import Waitpage from "../../../Waitpage";
 import ProductBuyafter from "./ProductBuyafter";
+import {  PC, Tablet } from "../../../MediaQuery"
 function Productdetailsub({content}) {
-  console.log(content,'contnetn')
-    const items = [
-        {
-          key: '1',
-          label: `상세제품설명`,
-          children:   <div
+
+  return (
+    <>
+    <div
+      className="acnhorwrap"
+    >
+         <PC>
+         <Anchor
+      
+      targetOffset={120}
+      offsetTop={120}
+        direction="horizontal"
+        items={[
+          {
+            key: 'part-1',
+            href: '#part-1',
+            title: '상세제품설명 1',
+          },
+          {
+            key: 'part-2',
+            href: '#part-2',
+            title: '관련자료',
+          },
+          {
+            key: 'part-3',
+            href: '#part-3',
+            title: '제품후기',
+          },
+          {
+            key: 'part-4',
+            href: '#part-4',
+            title: '기술지원문의',
+          },
+          {
+            key: 'part-5',
+            href: '#part-5',
+            title: '교환/반품/배송정보',
+          }
+        ]}
+      />
+
+         </PC>
+         <Tablet>
+         <Anchor
+      
+      targetOffset={52}
+      offsetTop={52}
+        direction="horizontal"
+        items={[
+          {
+            key: 'part-1',
+            href: '#part-1',
+            title: '상세제품설명 1',
+          },
+          {
+            key: 'part-2',
+            href: '#part-2',
+            title: '관련자료',
+          },
+          {
+            key: 'part-3',
+            href: '#part-3',
+            title: '제품후기',
+          },
+          {
+            key: 'part-4',
+            href: '#part-4',
+            title: '기술지원문의',
+          },
+          {
+            key: 'part-5',
+            href: '#part-5',
+            title: '교환/반품/배송정보',
+          }
+        ]}
+      />
+
+         </Tablet>
+     
+    </div>
+    <div className="partwrap">
+        <div
+         id="part-1"
           dangerouslySetInnerHTML={{__html: content&&content[0].content}}
           > 
           </div>,  
+          <div
+           id="part-2"
+          style={{position:'relative',minHeight:'150px'}}
           
-        },
-        {
-          key: '2',
-          label: `관련자료`,
-          children:<Waitpage/>,
-        },
-        {
-            key: '3',
-            label: `제품후기`,
-            children:<ProductBuyafter data={content}/>,
-          },
-          {
-            key: '4',
-            label: `기술지원문의`,
-            children: <Waitpage/>,
-          },
-          {
-            key: '5',
-            label: `교환/반품/배송정보`,
-            children: <Productdelivery/>,
-          },
-      ];
-      const {
-        token: { colorBgContainer },
-      } = theme.useToken();
-      const renderTabBar = (props, DefaultTabBar) => (
-        <StickyBox
-        offsetTop={120}
-        offsetBottom={20}
-          style={{
-            zIndex: 1,
-          }}
-        >
-          <DefaultTabBar
-            {...props}
-            style={{
-              background: colorBgContainer,
-            }}
-          />
-        </StickyBox>
-      );
-  return (
-
-    <Tabs
-                
-    items={items}
-    defaultActiveKey="1" 
-    renderTabBar={renderTabBar} 
-/>
+          >
+            <Waitpage/>
+          </div>
+      <div id="part-3">
+      <ProductBuyafter data={content}/>
+      </div>
+      <div
+           id="part-4"
+          style={{position:'relative',minHeight:'150px'}}
+          
+          >
+            <Waitpage/>
+          </div>
+      
+       <div id="part-5">
+       <Productdelivery/>
+      </div>
+    </div>
+  </>
     
   );
 }
